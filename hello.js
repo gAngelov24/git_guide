3f510e7bcad578a1cc07ae331b0968abc9b3ecea
George Angelov <georgeangelov2@gmail.com>
2025-03-16 14:45:08 -0500

    Another commit because I screwed up

--- hello.js
+++ hello.js
@@ -1,1 +1,2 @@
-console.log('Hello git!');
+console.log('Hello git!'); 
+console.log("this is a second attempt at learning git");
--- test.js
+++ test.js
@@ -1,1 +1,2 @@
 console.log('test');
+console.log("another test because I messed up the first time");
